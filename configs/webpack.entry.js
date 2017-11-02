6747a407a18392638f5bf538a709ee12f63b8304
anonymous0374 <377738226@qq.com>
2017-11-02 15:53:07 +0800

    generate entries by function

--- configs/webpack.entry.js
+++ configs/webpack.entry.js
@@ -10,21 +10,16 @@ const AppConfig = require('./app.config.js')
 function getEntries(config) {
   var i = 0,
     len = config.entryPaths.length,
-    entries = []
+    entries = {}
 
   for (i = 0; i < len; i += 1) {
-    let entry = {}
-    entry[config.entryNames[i]] = path.resolve(__dirname, config.entryNames[i])
+    entries[config.entryNames[i]] = path.resolve(__dirname, config.entryPaths[i])
   }
 
   return entries
 }
 
 module.exports = {
-  // ...getEntries(AppConfig)
+  entry: getEntries(AppConfig)
 
-  entry: {
-    index: path.resolve(__dirname, '../src/pages/index/index.js'),
-    mypage: path.resolve(__dirname, '../src/pages/mypage/index.js')
-  }
 }
